d4e4602b8f03e62c26338425755f4c5bb6a2b89c
weird-alex <athanasiadisalex2@gmail.com>
2021-08-08 23:29:22 +0300

    program is now a plotter

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 braille: braille.c
-	gcc `pkg-config --cflags ncursesw` braille.c `pkg-config --libs ncursesw` -o exec
+	gcc `pkg-config --cflags ncursesw` braille.c `pkg-config --libs ncursesw` -lm -o exec
 
 clean:
 	rm exec
--- braille.c
+++ braille.c
@@ -6,6 +6,7 @@
 #include <wchar.h>
 #include <locale.h>
 #include <time.h>
+#include <math.h>
 
 char *bytes_by_bits[5][5];
 int len[5][5];
@@ -35,6 +36,10 @@ wchar_t random_braille(int u_dots, int l_dots) {
     unsigned char offset = bytes_by_bits[u_dots][l_dots][rand() % len[u_dots][l_dots]];
     wchar_t braille = 0x2800 + offset;
     return braille;
+}
+
+float f(float x, float y) {
+    return sin(10*x -7*y);
 }
 
 int main() {
@@ -80,15 +85,43 @@ int main() {
 
     start_color();
     /*-DISPLAY-CODE-GOES-HERE-*/
-    init_pair(1, COLOR_CYAN, COLOR_BLUE);
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
 
     clear();
+
+    float F[2*LINES][COLS];
+
+    float maxF = f(0,0);
+    float minF = f(0,0);
+    for(int i = 0 ; i < 2*LINES ; i++) {
+        for(int j = 0 ; j < COLS ; j++) {
+            float y = (float)i/LINES - 1;
+            float x = 2*(float)j/COLS - 1;
+            
+            F[i][j] = f(x,y);
+
+            if(F[i][j] > maxF) {
+                maxF = F[i][j];
+            } else if(F[i][j] < minF) {
+                minF = F[i][j];
+            }
+        }
+    }
+
+    // normalizing
+    for(int i = 0 ; i < 2*LINES ; i++) {
+        for(int j = 0 ; j < COLS ; j++) {
+            F[i][j] = (F[i][j] - minF)/(maxF - minF);
+        }
+    }
+    
     attron(COLOR_PAIR(1));
-    for(int y = 0 ; y < LINES ; y++) {
-        for(int x = 0 ; x < COLS ; x++) {
-            int k = fround(4*((float)x/COLS));
-            wchar_t str[] = {random_braille(k,k), L'\0'};
-            mvaddwstr(y, x, str);
+    for(int i = 0 ; i < LINES ; i++) {
+        for(int j = 0 ; j < COLS ; j++) {
+            int u = fround(4*F[2*i][j]);
+            int l = fround(4*F[2*i+1][j]);
+            wchar_t str[] = {random_braille(u,l), L'\0'};
+            mvaddwstr(i, j, str);
         }
     }
     attroff(COLOR_PAIR(1));
